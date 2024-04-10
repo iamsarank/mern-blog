4173ec9d741902a55459230f3076a7a3838b3a30
saravanakumar <iamsarank@gmail.com>
2024-04-10 17:42:41 +0530

    delete user api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -50,6 +50,7 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(400, 'Invalid password'));
     }
     const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
       process.env.JWT_SECRET
     );
 
@@ -71,7 +72,8 @@ export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id },
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
         process.env.JWT_SECRET
       );
       const { password, ...rest } = user._doc;
@@ -95,7 +97,8 @@ export const google = async (req, res, next) => {
         profilePicture: googlePhotoUrl,
       });
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id },
+      const token = jwt.sign(
+        { id: newUser._id, isAdmin: newUser.isAdmin },
         process.env.JWT_SECRET
       );
       const { password, ...rest } = newUser._doc;
